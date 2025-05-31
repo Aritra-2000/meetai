6a7e9e991215bbf1a71b1475288db8fc32e1548b
Aritra <aritra.paul555@gmail.com>
2025-05-31 18:13:26 +0530

    4:authentication ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+
+import { SignInView } from "@/modules/auth/ui/views/sign-in-views";
+
+const Page = () =>{
+    console.log("Signin Page");
+    return(
+       <SignInView/>
+    );
+}
+
+export default Page;
