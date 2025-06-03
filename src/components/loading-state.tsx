cfa0412fd9c1ef02e4cb33ddc359364bdcf52a6d
Aritra <aritra.paul555@gmail.com>
2025-06-03 14:39:01 +0530

    9:Agents Setup

---
+++ src/components/loading-state.tsx
@@ -0,0 +1,28 @@
+
+import { Loader2Icon } from "lucide-react";
+
+
+interface Props{
+    title: string,
+    description: string
+}
+
+export const LoadingState = ({
+    title,
+    description
+}: Props) =>{
+    return (
+        <div className="py-4 px-8 flex flex-1 items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-y-6 bg-white rounded-lg p-10 shadow-sm">
+                <Loader2Icon 
+                    className="w-6 h-6 animate-spin text-primary" 
+                    aria-label="Loading"
+                />
+                <div className="flex flex-col gap-y-2 text-center">
+                    <h6 className="text-lg font-medium">{title}</h6>
+                    <p className="text-sm">{description}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
